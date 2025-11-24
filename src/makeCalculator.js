b5cd884e595d31aa369b0badeafcd466d0458463
Andriy <andriit35@gmail.com>
2025-11-24 13:47:18 +0200

    complete task

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,33 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+    reset() {
+      calculator.result = 0;
+
+      return calculator;
+    },
+    add: (number = 0) => {
+      calculator.result += number;
+    },
+    subtract: (number = 0) => {
+      calculator.result -= number;
+    },
+    multiply: (number = 1) => {
+      calculator.result *= number;
+    },
+    divide: (number = 1) => {
+      calculator.result /= number;
+    },
+    operate(method, number) {
+      method(number);
+
+      return calculator;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
